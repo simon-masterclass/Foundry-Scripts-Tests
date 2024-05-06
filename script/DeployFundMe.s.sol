3ff71fa80b529fb66accb9831a5e5297cabf5ff5
Simon Ionashku <simon.ionashku@gmail.com>
2024-05-06 17:34:28 -0400

    [08] - Test Script Refactor

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -7,17 +7,17 @@ import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
     // contract address for Chainlink price feed oracles on different networks
-    enum priceFeedAddr { BASE_SEPOLIA, MAINNET_SEPOLIA, AVALANCHE_TESTNET}
 
-    mapping(priceFeedAddr => address) public priceFeedsEthUsd;
-
-    function run() external {   
-        priceFeedsEthUsd[priceFeedAddr.BASE_SEPOLIA] = 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1;
-        priceFeedsEthUsd[priceFeedAddr.MAINNET_SEPOLIA] = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-        priceFeedsEthUsd[priceFeedAddr.AVALANCHE_TESTNET] = 0x86d67c3D38D2bCeE722E601025C25a575021c6EA;
+    function run() external returns (FundMe){   
+        // contract address for Chainlink price feed oracles on different networks
+        address BASE_SEPOLIA = 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1;
+        // address MAINNET_SEPOLIA = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        // address AVALANCHE_TESTNET = 0x86d67c3D38D2bCeE722E601025C25a575021c6EA;
 
         vm.startBroadcast();
-            new FundMe(priceFeedsEthUsd[priceFeedAddr.BASE_SEPOLIA]);
+            FundMe fundMe = new FundMe(BASE_SEPOLIA);
         vm.stopBroadcast();
+
+        return fundMe;
     } 
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,20 +3,15 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
-    // contract address for Chainlink price feed oracles on different networks
-    enum priceFeedAddr { BASE_SEPOLIA, MAINNET_SEPOLIA, AVALANCHE_TESTNET}
-    mapping(priceFeedAddr => address) public priceFeedsEthUsd;
-
     FundMe public fundMe;
 
     function setUp() public {
-        priceFeedsEthUsd[priceFeedAddr.BASE_SEPOLIA] = 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1;
-        priceFeedsEthUsd[priceFeedAddr.MAINNET_SEPOLIA] = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-        priceFeedsEthUsd[priceFeedAddr.AVALANCHE_TESTNET] = 0x86d67c3D38D2bCeE722E601025C25a575021c6EA;
-        
-        fundMe = new FundMe(priceFeedsEthUsd[priceFeedAddr.BASE_SEPOLIA]);
+        // fundMe = DeployFundMe.run();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function test_MinumumUSDis5() public view {
@@ -27,7 +22,7 @@ contract FundMeTest is Test {
         console.log("Owner is:", fundMe.i_owner());
         console.log("FundMeTest Address is:", address(this));
         console.log("Msg sender is:", msg.sender);
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     // function test_Fund() public {
